d90158117d3723e32fe49b17285a1314c8d969b5
Prince <princechauhan1992@hotmail.com>
2020-09-20 14:52:35 +0530

    added jest

--- sort.js
+++ sort.js
@@ -1,4 +1,12 @@
 function sort(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('Input should be an array');
+  }
+  arr.forEach((elem) => {
+    if (typeof elem !== 'number') {
+      throw new Error('Every element of input array should be a number');
+    }
+  });
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] > arr[j]) {
